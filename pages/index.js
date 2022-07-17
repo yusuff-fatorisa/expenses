958c2b773a61b883e58980746abbe36bc1d6a0a6
yusuff-fatorisa <bosun4real009@gmail.com>
2022-07-17 11:19:31 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// const fetchData = async () => {
+//    const data = await fetch('./data.json');
+//    const result = await data.json();
+//    console.log(result)
+// }
+
+// fetchData();
+
+// const myData = require("./data.js");
+// console.log("read successfully;")
